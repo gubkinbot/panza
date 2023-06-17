48ebf32e4d865d85162492b5d94cc759247f3280
Ivan Samorukov <50461542+gubkinbot@users.noreply.github.com>
2023-06-17 23:39:46 +0500

    включение сервиса OpenAI

--- bot/bot.py
+++ bot/bot.py
@@ -1,18 +1,25 @@
 import telebot
+import openai
 import os
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
 bot = telebot.TeleBot(os.getenv("PANZA_TG_TOKEN"))
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, 'Привет! Я расскажу тебе, что умею...')
+    bot.send_message(message.chat.id, 'Внутренний сервис для ЛУОК. CV и NLP в помощь.')
 
 @bot.message_handler(commands=['coffee'])
 def handle_start(message):
-    bot.send_message(message.chat.id, 'Кофе?')
+    bot.send_message(message.chat.id, 'Модуль анализа загрузки буфета. Баллы пробок, как в Яндекс.Картах.')
+
+@bot.message_handler(commands=['learn'])
+def handle_start(message):
+    bot.send_message(message.chat.id, 'Познавательный модуль. Три курса обучения: искусственный интеллект, кофе и нефтегаз. Подписка на AI-анализ статей SPE.')
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    bot.send_message(message.chat.id, message.text)
+    response = openai.ChatCompletion.create(model='gpt-3.5-turbo-16k-0613', messages=[{'role': 'system', 'content': 'Ты помощник ЛУОК'}, {'role': "user", "content": str(message.text)}])
+    bot.send_message(message.chat.id, response['choices'][0]['message']['content'])
 
 bot.polling()
